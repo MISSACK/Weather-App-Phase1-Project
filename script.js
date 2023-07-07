49d0f62584fad0b4373f02ea51cccbf9ff5d1125
Musa Issack <musa.m.issack@gmail.com>
2023-07-07 09:49:33 +0300

    updated html elements with fetched data

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ let weather = {
         const { speed } = data.wind;
         // Display temperature with one decimal point
         const formattedTemp = temp.toFixed(1);
-    }
 
-}
+        // Update the HTML elements with the fetched weather data
+        document.querySelector(".city-heading").innerText = "Weather in " + name;
+        document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temperature").innerText = formattedTemp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind-speed").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    }
+        // Get the user input from the search bar and fetch weather data
+    search: function () {
+            this.fetchWeather(document.querySelector(".search-input").value);
+    }      
+};
